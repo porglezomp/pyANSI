fe60bdcc8361c713056483713f9743b7b6e644ea
porglezomp <bionicle.dragon@gmail.com>
2014-05-30 09:26:48 -0400

    Create ANSI.py
    Add tons of colors to it
    Add ansi(str): Prints the escape character then the string, then flushes.
    Add goto(x, y): Moves the cursor to position x, y (1-based, upper-left based)
    Add color(n): Sets the color to n (should use one of the mess of variables for n, e.g. color(blue_bg))
    Add clear(): Clears the screen
    Add reset(): Removes all the special formatting (colors)

---
+++ ANSI.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+import sys
+
+# Modifiers
+bg = 10
+bright = 60
+# Colors
+black = 30
+red = 31
+green = 32
+yellow = 33
+blue = 34
+magenta = 35
+cyan = 36
+white = 37
+# Color mixes
+bright_black = bright+black
+bright_red = bright+red
+bright_green = bright+green
+bright_yellow = bright+yellow
+bright_blue = bright+blue
+bright_magenta = bright+magenta
+bright_cyan = bright+cyan
+bright_white = bright+white
+black_bg = black+bg
+red_bg = red+bg
+green_bg = green+bg
+yellow_bg = yellow+bg
+blue_bg = blue+bg
+magenta_bg = magenta+bg
+cyan_bg = cyan+bg
+white_bg = white+bg
+bright_black_bg = black_bg+bright
+bright_red_bg = red_bg+bright
+bright_green_bg = green_bg+bright
+bright_yellow_bg = yellow_bg+bright
+bright_black_bg = blue_bg+bright
+bright_magenta_bg = magenta_bg+bright
+bright_cyan_bg = cyan_bg+bright
+bright_white_bg = white_bg+bright
+
+def ansi(str):
+    sys.stdout.write("\033["+str)
+    sys.stdout.flush()
+
+def goto(x, y):
+    ansi(str(y)+";"+str(x)+"H")
+
+def color(n):
+    ansi(str(n)+"m")
+
+def clear():
+    goto(1, 1)
+    ansi("2J")
+
+def reset():
+    ansi("0m")
